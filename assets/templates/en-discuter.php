518e19bae6331815b598198b4cdbf37b4888fb01
devturcotte <jaylap26@icloud.com>
2025-01-18 14:15:12 -0500

    update nous-connaitre

--- assets/templates/en-discuter.php
+++ assets/templates/en-discuter.php
@@ -1,4 +1,5 @@
 <section class="en-discuter">
         <h2>Tu as d'autres questions? Contacte-nous pour en discuter!</h2>
         <button>Contactez-nous</button>
+        <img src="<?php echo get_template_directory_uri() ?>/assets/wp_media-library/ananas.svg" alt="Sticker" class="sticker">
 </section>
--- assets/templates/equipe.php
+++ assets/templates/equipe.php
@@ -1,3 +1,40 @@
-<section class="equipe">
+<section class="equipe-main-container">
     <h2>L'équipe derrière Oskar</h2>
+    <div class="hr"></div>
+    <div class="equipe-secondary-container">
+        <figure>
+            <img src="<?php echo get_template_directory_uri() ?>/assets/wp_media-library/bg-equipe-1.svg" alt="background">
+        </figure>
+        <img src="<?php echo get_template_directory_uri() ?>/assets/wp_media-library/ananas.svg" alt="Sticker" class="sticker">
+    
+        <div class="membres-nav">
+            <i class="fa-solid fa-chevron-left fa-xl"></i>
+            <ul>
+                <li><img src="<?php echo get_template_directory_uri() ?>/assets/wp_media-library/membre-equipe.png" alt="Membre"></li>
+                <li><img src="<?php echo get_template_directory_uri() ?>/assets/wp_media-library/membre-equipe.png" alt="Membre"></li>
+                <li><img src="<?php echo get_template_directory_uri() ?>/assets/wp_media-library/membre-equipe.png" alt="Membre"></li>
+                <li><img src="<?php echo get_template_directory_uri() ?>/assets/wp_media-library/membre-equipe.png" alt="Membre"></li>
+            </ul>
+            <i class="fa-solid fa-chevron-right fa-xl"></i>
+        </div>
+
+        <section class="membre">
+            <h3 class="nom">Team OSKAR</h3>
+            <div class="contenu">
+                <section class="fun-stats">
+                    <h4>Fun stats</h4>
+                </section>
+                <section class="anecdote">
+                    <h4>Anecdote:</h4>
+                    <p>Skate ipsum dolor sit amet, lip boned out shinner yeah death box Steve Rocco. 720 ho-ho nosegrind transfer pogo. Coper Tracker slam no comply boardslide. Indy g</p>
+                </section>
+                <section class="description">
+                    <h4>Description:</h4>
+                    <p>Skate ipsum dolor sit amet, lip boned out shinner yeah death box Steve Rocco. 720 ho-ho nosegrind transfer pogo. Coper Tracker slam no comply boardslide. Indy g</p>
+                </section>
+            </div>
+        </section>
+
+        <img src="<?php echo get_template_directory_uri() ?>/assets/images/logo-footer.png" alt="Logo Oskar" class="logo">
+    </div>
 </section>

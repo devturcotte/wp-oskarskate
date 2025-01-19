edd5fa417e6d60d1ed9f27ea2dba91b50c4e62ee
devturcotte <jaylap26@icloud.com>
2025-01-19 17:17:30 -0500

    update faq

--- assets/templates/faq.php
+++ assets/templates/faq.php
@@ -5,12 +5,18 @@
             <h3>Section 1</h3>
             <ul class="questions-container">
                 <li class="question">
-                    <h4>Question 1 ?</h4>
-                    <p>Réponse 1.</p>
+                    <div class="title">
+                        <h4>Question numéro 1 ?</h4>
+                        <i class="fa-solid fa-plus fa-lg"></i>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vel blandit ex, sit amet venenatis eros. Ut et libero vel elit venenatis dignissim non et nibh. Suspendisse vulputate feugiat est, quis pellentesque enim tempor ac. Sed pellentesque odio sem, a facilisis urna fermentum vitae. Donec vitae enim eu enim finibus tristique. Mauris vitae libero dictum, ullamcorper libero eu, lacinia est. Duis nec risus non sem volutpat ornare. </p>
                 </li>
                 <li class="question">
-                    <h4>Question 2 ?</h4>
-                    <p>Réponse 2.</p>
+                    <div class="title">
+                        <h4>Question numéro 2 ?</h4>
+                        <i class="fa-solid fa-plus fa-lg"></i>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vel blandit ex, sit amet venenatis eros. Ut et libero vel elit venenatis dignissim non et nibh. Suspendisse vulputate feugiat est, quis pellentesque enim tempor ac. Sed pellentesque odio sem, a facilisis urna fermentum vitae. Donec vitae enim eu enim finibus tristique. Mauris vitae libero dictum, ullamcorper libero eu, lacinia est. Duis nec risus non sem volutpat ornare. </p>
                 </li>
             </ul>
         </li>
@@ -18,12 +24,18 @@
             <h3>Section 2</h3>
             <ul class="questions-container">
                 <li class="question">
-                    <h4>Question 1 ?</h4>
-                    <p>Réponse 1.</p>
+                    <div class="title">
+                        <h4>Question numéro 3 ?</h4>
+                        <i class="fa-solid fa-plus fa-lg"></i>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vel blandit ex, sit amet venenatis eros. Ut et libero vel elit venenatis dignissim non et nibh. Suspendisse vulputate feugiat est, quis pellentesque enim tempor ac. Sed pellentesque odio sem, a facilisis urna fermentum vitae. Donec vitae enim eu enim finibus tristique. Mauris vitae libero dictum, ullamcorper libero eu, lacinia est. Duis nec risus non sem volutpat ornare. </p>
                 </li>
                 <li class="question">
-                    <h4>Question 2 ?</h4>
-                    <p>Réponse 2.</p>
+                    <div class="title">
+                        <h4>Question numéro 4 ?</h4>
+                        <i class="fa-solid fa-plus fa-lg"></i>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vel blandit ex, sit amet venenatis eros. Ut et libero vel elit venenatis dignissim non et nibh. Suspendisse vulputate feugiat est, quis pellentesque enim tempor ac. Sed pellentesque odio sem, a facilisis urna fermentum vitae. Donec vitae enim eu enim finibus tristique. Mauris vitae libero dictum, ullamcorper libero eu, lacinia est. Duis nec risus non sem volutpat ornare. </p>
                 </li>
             </ul>
         </li>

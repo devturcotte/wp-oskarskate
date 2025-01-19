be0e87f0599fcf2491887361890ac225d82f1d09
devturcotte <jaylap26@icloud.com>
2025-01-19 11:41:32 -0500

    update simpliquer

---
+++ assets/templates/benevolat.php
@@ -0,0 +1,8 @@
+<div class="benevolat">
+        <img src="<?php echo get_template_directory_uri() ?>/assets/wp_media-library/benevolat.png" alt="img">
+        <section>
+            <h2>Bénévolat</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+            <button>Je m'implique</button>
+        </section>
+</div>

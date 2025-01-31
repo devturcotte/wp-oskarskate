6931453692e8595c67ccae8f9849b6d3e174cee7
Maude Bélanger <info@becma.ca>
2025-01-31 12:19:04 -0500

    fix pour les conflits de posts

--- assets/templates/faq.php
+++ assets/templates/faq.php
@@ -22,6 +22,7 @@
                 <p class="reponse hidden"><?php echo $reponse; ?></p>
             </li>
         <?php
+            wp_reset_postdata();
             };
         } 
         else{ ?>
